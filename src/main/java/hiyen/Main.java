9142af56a80995a68998a375f1f6c032c971b0a0
jinkshower <askthem@naver.com>
2024-07-08 10:08:57 +0900

    add overloads

--- src/main/java/hiyen/Main.java
+++ src/main/java/hiyen/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		//add overloads to
+		//i want to force push and make commit
+		//2nd commit
+		//newnew commit
+		//fourth commit
 	}
 }
