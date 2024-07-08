bf803f64c7b4bf036f417acbbaaca3fe11d816d0
jinkshower <askthem@naver.com>
2024-07-08 11:04:54 +0900

    10:51 pr

---
+++ src/main/java/hiyen/Main.java
@@ -0,0 +1,17 @@
+package hiyen;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		//add overloads to
+		//i want to force push and make commit
+		//2nd commit
+		//newnew commit
+		//fourth commit
+
+		//original's update
+		//original's 10:53 update
+		//what if i created pr and origin updated?
+	}
+}
